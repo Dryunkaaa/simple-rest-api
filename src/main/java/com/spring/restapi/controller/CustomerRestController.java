e5cc811ae03ec9d2cfda09bfb50f4e8d92080b22
Andrey <dryunkaaa@gmail.com>
2020-10-01 23:15:07 +0300

    Created customer mapped object

--- src/main/java/com/spring/restapi/controller/CustomerRestController.java
+++ src/main/java/com/spring/restapi/controller/CustomerRestController.java
@@ -1,4 +1,8 @@
 package com.spring.restapi.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "/customer/api/")
 public class CustomerRestController {
+
 }

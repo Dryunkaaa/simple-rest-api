49cf8680a3162d9335e698d2b5ad29f35b9461f2
Andrey <dryunkaaa@gmail.com>
2020-10-03 16:04:29 +0300

    Added mappings

--- src/main/java/com/spring/restapi/controller/CustomerRestController.java
+++ src/main/java/com/spring/restapi/controller/CustomerRestController.java
@@ -1,8 +1,82 @@
 package com.spring.restapi.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.restapi.domain.Customer;
+import com.spring.restapi.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RestController(value = "/customer/api/")
+import javax.print.attribute.standard.Media;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers/")
 public class CustomerRestController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") Long customerId) {
+        if (customerId == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Customer customer = customerService.getById(customerId);
+
+        if (customer.getFirstName() == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Customer>> getCustomers() {
+        List<Customer> customers = customerService.getAll();
+
+        if (customers.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") Long id) {
+        Customer customer = customerService.getById(id);
+
+        if (customer.getFirstName() == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        customerService.remove(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer) {
+        if (customer == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        this.customerService.save(customer);
+
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer) {
+        if (customer == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        this.customerService.save(customer);
+
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
 
 }
